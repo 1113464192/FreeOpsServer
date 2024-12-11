bcfb8cbb912588dbfbb702a3b9336be626bb57e7
FanQihang <96534909+1113464192@users.noreply.github.com>
2024-12-11 15:26:03 +0800

    prevent

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -4,3 +4,12 @@ const (
 	// 自定义SSH命令错误返回值
 	SSHCustomCmdError = 9999
 )
+
+// 不记录到用户操作的post请求路径
+var SkipLoggingPostPaths = []string{
+	"/ops/commands",
+	"/auth/login",
+	"/auth/refreshToken",
+	"/ops/run-task-check-script",
+	"/users/logout",
+}
--- internal/middleware/user_record.go
+++ internal/middleware/user_record.go
@@ -45,58 +45,75 @@ func (r responseBodyWriter) Write(b []byte) (int, error) {
 	return r.ResponseWriter.Write(b)
 }
 
+func shouldSkipLogging(method, path string) bool {
+	if method == "GET" {
+		return true
+	}
+
+	if method == "POST" {
+		for _, p := range consts.SkipLoggingPostPaths {
+			if path == p {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func UserRecord() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.Request.Method != "GET" {
-			var body []byte
-			user, err := util.GetClaimsUser(c)
-			if err != nil {
-				c.JSON(200, api.Response{
-					Code: consts.SERVICE_MODAL_LOGOUT_CODE,
-					Msg:  err.Error(),
-				})
-				return
-			}
-			body, err = io.ReadAll(c.Request.Body)
-			if err != nil {
-				logger.Log().Error("userRecord", "记录用户请求body失败", err)
-				c.Abort()
-				return
-			} else {
-				c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
-			}
-			record := &model.UserRecord{
-				Ip:       c.ClientIP(),
-				Method:   c.Request.Method,
-				Path:     c.Request.URL.Path,
-				Agent:    c.Request.UserAgent(),
-				Body:     string(body),
-				UserID:   user.ID,
-				Username: user.Username,
-			}
-
-			writer := writerPool.Get().(*responseBodyWriter)
-			writer.ResponseWriter = c.Writer
-			writer.body.Reset()
-			c.Writer = writer
-
-			startNow := time.Now().Local()
+		if shouldSkipLogging(c.Request.Method, c.Request.URL.Path) {
 			c.Next()
-			record.Latency = time.Since(startNow)
-			record.Status = c.Writer.Status()
-			record.Resp = writer.body.String()
-			if err = recordService.CreateRecord(record); err != nil {
-				logger.Log().Error("userRecord", "创建用户记录失败", err)
-			}
-			if len(record.Resp) > 1024 {
-				newBody := respPool.Get().([]byte)
-				copy(newBody, record.Resp)
-				record.Resp = string(newBody)
-				defer respPool.Put(newBody)
-			}
+			return
+		}
 
+		var body []byte
+		user, err := util.GetClaimsUser(c)
+		if err != nil {
+			c.JSON(200, api.Response{
+				Code: consts.SERVICE_MODAL_LOGOUT_CODE,
+				Msg:  err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		body, err = io.ReadAll(c.Request.Body)
+		if err != nil {
+			logger.Log().Error("userRecord", "记录用户请求body失败", err)
+			c.Abort()
+			return
 		} else {
-			c.Next()
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+		}
+		record := &model.UserRecord{
+			Ip:       c.ClientIP(),
+			Method:   c.Request.Method,
+			Path:     c.Request.URL.Path,
+			Agent:    c.Request.UserAgent(),
+			Body:     string(body),
+			UserID:   user.ID,
+			Username: user.Username,
+		}
+
+		writer := writerPool.Get().(*responseBodyWriter)
+		writer.ResponseWriter = c.Writer
+		writer.body.Reset()
+		c.Writer = writer
+
+		startNow := time.Now().Local()
+		c.Next()
+		record.Latency = time.Since(startNow)
+		record.Status = c.Writer.Status()
+		record.Resp = writer.body.String()
+		if err = recordService.CreateRecord(record); err != nil {
+			logger.Log().Error("userRecord", "创建用户记录失败", err)
+		}
+		if len(record.Resp) > 1024 {
+			newBody := respPool.Get().([]byte)
+			copy(newBody, record.Resp)
+			record.Resp = string(newBody)
+			defer respPool.Put(newBody)
 		}
 
 	}
