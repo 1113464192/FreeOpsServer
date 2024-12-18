b51829c3a7fc957d16cc82fd970fefde94d839ec
FanQihang <96534909+1113464192@users.noreply.github.com>
2024-12-18 14:56:17 +0800

    update README.md

--- init.sql
+++ init.sql
@@ -54,37 +54,37 @@ UNLOCK TABLES;
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
 INSERT INTO `menu` VALUES
-(1,'0000-00-00 00:00:00.000','2024-07-15 15:03:36.000',NULL,1,0,2,'首页','home','/home','layout.base$view.home',1,'route.home','mdi:monitor-dashboard',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(2,'2024-07-11 14:45:59.000','2024-09-29 16:52:24.000',NULL,1,0,1,'功能','function','/function','layout.base',2,'route.function','icon-park-outline:all-application',1,0,1,NULL,'0',0,0,NULL,NULL,NULL),
-(3,'0000-00-00 00:00:00.000','2024-07-15 15:49:41.000',NULL,1,2,2,'多标签页','function_multi-tab','/function/multi-tab','view.function_multi-tab',1,'route.function_multi-tab','ic:round-tab',1,1,1,0,'0',0,0,NULL,NULL,NULL),
-(4,'2024-07-11 16:05:43.000','2024-07-11 16:05:43.000',NULL,1,2,2,'标签页','function_tab','/function/tab','view.function_tab',2,'route.function_tab','ic:round-tab',1,0,1,NULL,'0',0,0,NULL,NULL,NULL),
-(5,'2024-07-11 16:49:02.000','2024-07-11 16:49:02.000',NULL,1,0,1,'异常页','exception','/exception','layout.base',3,'route.exception','ant-design:exception-outlined',1,0,1,NULL,'0',1,0,NULL,NULL,NULL),
-(6,'2024-07-11 17:02:36.000','2024-07-11 17:02:36.000',NULL,1,5,2,'403','exception_403','/exception/403','view.403',1,'route.exception_403','ic:baseline-block',1,0,1,NULL,'0',1,0,NULL,NULL,NULL),
-(7,'2024-07-11 17:13:35.000','2024-07-11 17:13:35.000',NULL,1,5,2,'404','exception_404','/exception/404','view.404',2,'route.exception_404','ic:baseline-web-asset-off',1,0,1,NULL,'0',1,0,NULL,NULL,NULL),
-(8,'2024-07-11 17:16:37.000','2024-07-11 17:16:37.000',NULL,1,5,2,'500','exception_500','/exception/500','view.500',3,'route.exception_500','ic:baseline-wifi-off',1,0,1,NULL,'0',1,0,NULL,NULL,NULL),
-(9,'2024-07-11 17:50:50.000','2024-10-21 15:48:08.000',NULL,1,0,1,'系统管理','manage','/manage','layout.base',8,'route.manage','carbon:cloud-service-management',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(10,'2024-07-11 19:33:45.000','2024-07-15 15:49:41.000',NULL,1,9,2,'用户管理','manage_user','/manage/user','view.manage_user',1,'route.manage_user','ic:round-manage-accounts',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(11,'2024-07-11 19:38:52.000','2024-07-15 15:49:41.000',NULL,1,9,2,'角色管理','manage_role','/manage/role','view.manage_role',2,'route.manage_role','carbon:user-role',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(12,'2024-07-11 19:40:01.000','2024-08-15 14:14:54.000',NULL,1,9,2,'菜单管理','manage_menu','/manage/menu','view.manage_menu',3,'route.manage_menu','material-symbols:route',1,0,0,NULL,'1',0,0,NULL,NULL,NULL),
-(13,'2024-07-11 20:48:28.000','2024-08-30 17:58:25.000',NULL,1,9,2,'用户详情','manage_user-detail','/manage/user-detail/:id','view.manage_user-detail',4,'route.manage_user-detail','bxs:user-detail',1,0,1,0,'0',0,0,'true',NULL,NULL),
-(16,'2024-08-15 20:19:34.000','2024-08-15 20:19:34.000',NULL,1,0,2,'内嵌页面','iframe-page','/iframe-page/:url','layout.base$view.iframe-page',5,'route.iframe-page',NULL,1,0,1,NULL,'1',1,0,'true',NULL,NULL),
-(17,'2024-08-16 16:46:58.000','2024-08-16 16:46:58.000',NULL,1,0,2,'个人中心','user-center','/user-center','layout.base$view.user-center',6,'route.user-center',NULL,1,0,1,NULL,'0',0,0,NULL,NULL,NULL),
-(18,'2024-08-19 11:30:38.000','2024-08-19 11:30:38.000',NULL,1,0,2,'登录','login','/login/:module(pwd-login|code-login|register|reset-pwd|bind-wechat)?','layout.blank$view.login',7,'route.login',NULL,1,0,1,NULL,'0',1,0,'true',NULL,NULL),
-(19,'2024-09-05 15:00:00.000','2024-09-05 15:28:26.000',NULL,1,9,2,'API管理','manage_api','/manage/api','view.manage_api',5,'route.manage_api','hugeicons:api',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(20,'2024-09-06 15:57:19.000','2024-09-06 15:57:32.000',NULL,1,9,2,'用户操作记录','manage_user-record','/manage/user-record','view.manage_user-record',6,'route.manage_user-record','octicon:log-24',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(21,'2024-09-24 11:03:40.000','2024-10-21 15:48:13.000',NULL,1,0,1,'资产管理','asset','/asset','layout.base',9,'route.asset','fluent-mdl2:fixed-asset-management',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(22,'2024-09-24 11:09:44.000','2024-09-24 11:10:17.000',NULL,1,21,2,'项目管理','asset_project','/asset/project','view.asset_project',1,'route.asset_project','arcticons:projectm',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(23,'2024-09-27 14:47:51.000','2024-09-27 14:47:51.000',NULL,1,21,2,'服务器管理','asset_host','/asset/host','view.asset_host',2,'route.asset_host','clarity:host-group-solid',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(24,'2024-09-29 14:01:28.000','2024-09-29 14:08:18.000',NULL,1,0,1,'文档','document','/document','layout.base',4,'route.document','et:document',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(25,'2024-09-29 17:26:20.000','2024-09-29 17:52:45.000',NULL,1,24,2,'项目文档(内链)','document_project','/document/project','view.iframe-page',1,'route.document_project','gala:file-document',1,0,0,NULL,'0',0,0,'{\"url\":\"https://github.com/1113464192/FreeOpsServer\"}',NULL,NULL),
-(26,'2024-09-29 17:51:00.000','2024-09-29 17:51:00.000',NULL,1,24,2,'项目文档(外链)','document_project-link','/document/project-link','',2,'route.document_project-link','gala:file-document',1,0,0,NULL,'0',0,0,NULL,NULL,'https://github.com/1113464192/FreeOpsClient'),
-(27,'2024-10-21 15:50:29.000','2024-10-21 15:50:29.000',NULL,1,0,1,'运维操作管理','ops-manage','/ops-manage','layout.base',10,'route.ops-manage','carbon:operations-field',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(28,'2024-10-21 15:57:56.000','2024-10-21 15:57:56.000',NULL,1,27,2,'游戏管理','ops-manage_game','/ops-manage/game','view.ops-manage_game',1,'route.ops-manage_game','tabler:businessplan',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(29,'2024-11-21 13:38:31.000','2024-11-21 13:38:31.000',NULL,1,27,2,'操作模板管理','ops-manage_template','/ops-manage/template','view.ops-manage_template',2,'route.ops-manage_template','tdesign:template',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(30,'2024-11-26 16:57:56.000','2024-11-26 16:57:56.000',NULL,1,27,2,'参数模板管理','ops-manage_param-template','/ops-manage/param-template','view.ops-manage_param-template',3,'route.ops-manage_param-template','arcticons:param',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(31,'2024-11-29 10:35:03.000','2024-11-29 10:35:03.000',NULL,1,27,2,'任务管理','ops-manage_task','/ops-manage/task','view.ops-manage_task',4,'route.ops-manage_task','tdesign:task',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(32,'2024-12-03 14:56:02.000','2024-12-03 14:56:53.000',NULL,1,27,2,'审批任务','ops-manage_approve-task','/ops-manage/approve-task','view.ops-manage_approve-task',5,'route.ops-manage_approve-task','material-symbols-light:order-approve-outline-rounded',1,0,0,NULL,'0',0,0,NULL,NULL,NULL),
-(33,'2024-12-05 11:23:56.000','2024-12-05 11:24:31.000',NULL,1,27,2,'任务日志','ops-manage_task-log','/ops-manage/task-log','view.ops-manage_task-log',6,'route.ops-manage_task-log','mdi:math-log',1,0,0,NULL,'0',0,0,NULL,NULL,NULL);
+(1,'0000-00-00 00:00:00.000','2024-07-15 15:03:36.000',NULL,1,0,2,'首页','home','/home','layout.base$view.home',1,'route.home','mdi:monitor-dashboard',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(2,'2024-07-11 14:45:59.000','2024-09-29 16:52:24.000',NULL,1,0,1,'功能','function','/function','layout.base',2,'route.function','icon-park-outline:all-application',1,0,1,NULL,0,0,0,NULL,NULL,NULL),
+(3,'0000-00-00 00:00:00.000','2024-07-15 15:49:41.000',NULL,1,2,2,'多标签页','function_multi-tab','/function/multi-tab','view.function_multi-tab',1,'route.function_multi-tab','ic:round-tab',1,1,1,'function_tab',0,0,0,NULL,NULL,NULL),
+(4,'2024-07-11 16:05:43.000','2024-07-11 16:05:43.000',NULL,1,2,2,'标签页','function_tab','/function/tab','view.function_tab',2,'route.function_tab','ic:round-tab',1,0,1,NULL,0,0,0,NULL,NULL,NULL),
+(5,'2024-07-11 16:49:02.000','2024-07-11 16:49:02.000',NULL,1,0,1,'异常页','exception','/exception','layout.base',3,'route.exception','ant-design:exception-outlined',1,0,1,NULL,0,1,0,NULL,NULL,NULL),
+(6,'2024-07-11 17:02:36.000','2024-07-11 17:02:36.000',NULL,1,5,2,'403','exception_403','/exception/403','view.403',1,'route.exception_403','ic:baseline-block',1,0,1,NULL,0,1,0,NULL,NULL,NULL),
+(7,'2024-07-11 17:13:35.000','2024-07-11 17:13:35.000',NULL,1,5,2,'404','exception_404','/exception/404','view.404',2,'route.exception_404','ic:baseline-web-asset-off',1,0,1,NULL,0,1,0,NULL,NULL,NULL),
+(8,'2024-07-11 17:16:37.000','2024-07-11 17:16:37.000',NULL,1,5,2,'500','exception_500','/exception/500','view.500',3,'route.exception_500','ic:baseline-wifi-off',1,0,1,NULL,0,1,0,NULL,NULL,NULL),
+(9,'2024-07-11 17:50:50.000','2024-10-21 15:48:08.000',NULL,1,0,1,'系统管理','manage','/manage','layout.base',8,'route.manage','carbon:cloud-service-management',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(10,'2024-07-11 19:33:45.000','2024-07-15 15:49:41.000',NULL,1,9,2,'用户管理','manage_user','/manage/user','view.manage_user',1,'route.manage_user','ic:round-manage-accounts',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(11,'2024-07-11 19:38:52.000','2024-07-15 15:49:41.000',NULL,1,9,2,'角色管理','manage_role','/manage/role','view.manage_role',2,'route.manage_role','carbon:user-role',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(12,'2024-07-11 19:40:01.000','2024-08-15 14:14:54.000',NULL,1,9,2,'菜单管理','manage_menu','/manage/menu','view.manage_menu',3,'route.manage_menu','material-symbols:route',1,0,0,NULL,1,0,0,NULL,NULL,NULL),
+(13,'2024-07-11 20:48:28.000','2024-08-30 17:58:25.000',NULL,1,9,2,'用户详情','manage_user-detail','/manage/user-detail/:id','view.manage_user-detail',4,'route.manage_user-detail','bxs:user-detail',1,0,1,'manage_user',0,0,0,'true',NULL,NULL),
+(16,'2024-08-15 20:19:34.000','2024-08-15 20:19:34.000',NULL,1,0,2,'内嵌页面','iframe-page','/iframe-page/:url','layout.base$view.iframe-page',5,'route.iframe-page',NULL,1,0,1,NULL,1,1,0,'true',NULL,NULL),
+(17,'2024-08-16 16:46:58.000','2024-08-16 16:46:58.000',NULL,1,0,2,'个人中心','user-center','/user-center','layout.base$view.user-center',6,'route.user-center',NULL,1,0,1,NULL,0,0,0,NULL,NULL,NULL),
+(18,'2024-08-19 11:30:38.000','2024-08-19 11:30:38.000',NULL,1,0,2,'登录','login','/login/:module(pwd-login|code-login|register|reset-pwd|bind-wechat)?','layout.blank$view.login',7,'route.login',NULL,1,0,1,NULL,0,1,0,'true',NULL,NULL),
+(19,'2024-09-05 15:00:00.000','2024-09-05 15:28:26.000',NULL,1,9,2,'API管理','manage_api','/manage/api','view.manage_api',5,'route.manage_api','hugeicons:api',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(20,'2024-09-06 15:57:19.000','2024-09-06 15:57:32.000',NULL,1,9,2,'用户操作记录','manage_user-record','/manage/user-record','view.manage_user-record',6,'route.manage_user-record','octicon:log-24',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(21,'2024-09-24 11:03:40.000','2024-10-21 15:48:13.000',NULL,1,0,1,'资产管理','asset','/asset','layout.base',9,'route.asset','fluent-mdl2:fixed-asset-management',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(22,'2024-09-24 11:09:44.000','2024-09-24 11:10:17.000',NULL,1,21,2,'项目管理','asset_project','/asset/project','view.asset_project',1,'route.asset_project','arcticons:projectm',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(23,'2024-09-27 14:47:51.000','2024-12-17 10:16:40.000',NULL,1,21,2,'服务器管理','asset_host','/asset/host','view.asset_host',2,'route.asset_host','clarity:host-group-solid',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(24,'2024-09-29 14:01:28.000','2024-09-29 14:08:18.000',NULL,1,0,1,'文档','document','/document','layout.base',4,'route.document','et:document',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(25,'2024-09-29 17:26:20.000','2024-09-29 17:52:45.000',NULL,1,24,2,'项目文档(内链)','document_project','/document/project','view.iframe-page',1,'route.document_project','gala:file-document',1,0,0,NULL,0,0,0,'{\"url\":\"https://github.com/1113464192/FreeOpsServer\"}',NULL,NULL),
+(26,'2024-09-29 17:51:00.000','2024-09-29 17:51:00.000',NULL,1,24,2,'项目文档(外链)','document_project-link','/document/project-link','',2,'route.document_project-link','gala:file-document',1,0,0,NULL,0,0,0,NULL,NULL,'https://github.com/1113464192/FreeOpsClient'),
+(27,'2024-10-21 15:50:29.000','2024-10-21 15:50:29.000',NULL,1,0,1,'运维操作管理','ops-manage','/ops-manage','layout.base',10,'route.ops-manage','carbon:operations-field',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(28,'2024-10-21 15:57:56.000','2024-10-21 15:57:56.000',NULL,1,27,2,'游戏管理','ops-manage_game','/ops-manage/game','view.ops-manage_game',1,'route.ops-manage_game','tabler:businessplan',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(29,'2024-11-21 13:38:31.000','2024-11-21 13:38:31.000',NULL,1,27,2,'操作模板管理','ops-manage_template','/ops-manage/template','view.ops-manage_template',2,'route.ops-manage_template','tdesign:template',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(30,'2024-11-26 16:57:56.000','2024-11-26 16:57:56.000',NULL,1,27,2,'参数模板管理','ops-manage_param-template','/ops-manage/param-template','view.ops-manage_param-template',3,'route.ops-manage_param-template','arcticons:param',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(31,'2024-11-29 10:35:03.000','2024-11-29 10:35:03.000',NULL,1,27,2,'任务管理','ops-manage_task','/ops-manage/task','view.ops-manage_task',4,'route.ops-manage_task','tdesign:task',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(32,'2024-12-03 14:56:02.000','2024-12-03 14:56:53.000',NULL,1,27,2,'审批任务','ops-manage_approve-task','/ops-manage/approve-task','view.ops-manage_approve-task',5,'route.ops-manage_approve-task','material-symbols-light:order-approve-outline-rounded',1,0,0,NULL,0,0,0,NULL,NULL,NULL),
+(33,'2024-12-05 11:23:56.000','2024-12-05 11:24:31.000',NULL,1,27,2,'任务日志','ops-manage_task-log','/ops-manage/task-log','view.ops-manage_task-log',6,'route.ops-manage_task-log','mdi:math-log',1,0,0,NULL,0,0,0,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -220,6 +220,28 @@ INSERT INTO `api` VALUES
 (84,'2024-12-12 15:14:27.000','2024-12-12 15:14:27.000',NULL,'/api/games/status','PATCH','games','更新游戏状态');
 /*!40000 ALTER TABLE `api` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping data for table `button`
+--
+
+LOCK TABLES `button` WRITE;
+/*!40000 ALTER TABLE `button` DISABLE KEYS */;
+INSERT INTO `button` VALUES
+(21,'webssh','开发用户没个人密钥，就在这默认使用freeOPS配置文件的用户与密钥(只提供ls、less等权限比较安全)',23);
+/*!40000 ALTER TABLE `button` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `role_button`
+--
+
+LOCK TABLES `role_button` WRITE;
+/*!40000 ALTER TABLE `role_button` DISABLE KEYS */;
+INSERT INTO `role_button` VALUES
+(1,21);
+/*!40000 ALTER TABLE `role_button` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -229,4 +251,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-12  7:34:35
+-- Dump completed on 2024-12-18  6:53:21
